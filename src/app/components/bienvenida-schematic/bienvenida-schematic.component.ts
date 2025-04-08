a935909e42e81f40ac2945abc62ec4da638b3307
Kendal Sosa <kendal.alfonso37@gmail.com>
2025-04-07 19:48:35 -0600

    Generando primeros componentes.

---
+++ src/app/components/bienvenida-schematic/bienvenida-schematic.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bienvenida-schematic',
+  imports: [],
+  templateUrl: './bienvenida-schematic.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BienvenidaSchematicComponent { }
