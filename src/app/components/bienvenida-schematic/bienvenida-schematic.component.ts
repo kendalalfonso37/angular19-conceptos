c75ce1ced1c3b31e27591f292d716f4307f1b4db
Kendal Sosa <kendal.alfonso37@gmail.com>
2025-04-07 19:48:59 -0600

    Eliminado componente extra.

--- src/app/components/bienvenida-schematic/bienvenida-schematic.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-bienvenida-schematic',
-  imports: [],
-  templateUrl: './bienvenida-schematic.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class BienvenidaSchematicComponent { }
