2c63d25f160506ec5f0cbeac1531f8eba224c881
Kendal Sosa <kendal.alfonso37@gmail.com>
2025-04-07 21:16:21 -0600

    Clase de Componentes y Selectores.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BienvenidaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
