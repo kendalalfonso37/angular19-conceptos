4b37f705516a7a8a5bbdfedfed8c9d1f28dfeebf
Kendal Sosa <kendal.alfonso37@gmail.com>
2025-04-14 15:19:05 -0600

    Variables

--- src/app/components/variables/variables.component.ts
+++ src/app/components/variables/variables.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-variables',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './variables.component.html',
 })
-export class VariablesComponent { }
+export class VariablesComponent {
+  // One-Way Data Binding
+  myTitle: string = `Hola soy la variable myTitle`;
+  saldo: number = 123.32;
+
+  // Property Binding
+  link = 'https://v7.angular.cn/assets/images/logos/angular/angular.svg'
+
+  // Event Binding
+  textAreaValue = "";
+
+  // Creamos un EventEmitter local
+  customSubmit: EventEmitter<string> = new EventEmitter();
+
+  constructor() {
+    // Suscribirse al evento para mostrar el valor emitido
+    this.customSubmit.subscribe((value: string) => {
+      console.log('Valor emitido desde textarea:', value);
+    });
+  }
+
+  onSubmit() {
+    this.customSubmit.emit(this.textAreaValue); // emitir el valor del textarea
+  }
+
+  // Two-Way Data Binding
+  review = "Default review";
+  onSubmitReview() {
+    this.review = "Default review";
+  }
+}
