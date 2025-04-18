2efe3e44b1ffa276a7b661a8a8fa00575b4a81b1
Gabriela Miranda <mm15136@ues.edu.sv>
2025-04-18 10:40:33 -0600

    computed y effects.

--- src/app/components/signals/signals.component.ts
+++ src/app/components/signals/signals.component.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, computed, signal, effect } from '@angular/core';
 
 
 @Component({
   selector: 'app-signals',
-  imports: [],
+  imports: [DecimalPipe],
   templateUrl: './signals.component.html',
 })
 export class SignalsComponent {
+  // Signals
   contador = signal<number>(0);
 
   subtract() {
@@ -20,4 +22,15 @@ export class SignalsComponent {
   reset() {
     this.contador.set(0);
   }
+  // Computed
+  porcentaje = computed(() => this.contador() * 0.20);
+
+  firstName = signal('John');
+  lastName = signal('Doe');
+
+  fullName = computed(() => `${this.firstName()} ${this.lastName()}`);
+  // effect
+  myeffect = effect(() => {
+    console.log(`Valor del contador: ${this.contador()}`);
+  });
 }
